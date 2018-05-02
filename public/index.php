01a9925d4338ebccaca16b8ddebd52c669c1ea1e
Rodrigo Valceli Raimundo <rodrigo@ciga.sc.gov.br>
2018-05-02 01:57:46 +0000

    Added support for running in embedded server mode as a router script.

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
-
+if (php_sapi_name() == 'cli-server') {
+	// removes first slash '/' before checking 
+	// (absolute to relative path conversion)
+	if (file_exists(substr($_SERVER['REQUEST_URI'],1))) {
+		return false;		
+	}    
+}
 require_once('../components/Application.php');
 
 $app = new Application();
